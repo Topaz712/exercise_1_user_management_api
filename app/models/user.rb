4878b75bca25d4a0328ee9d141858fe861efd88a
Topaz712 <tzam712@gmail.com>
2024-02-01 15:04:16 -0600

    CHANGED:column name to :full_name in the User model and updated w/ migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { minimum: 3, maximum: 50 }
+  validates :full_name,  presence: true, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { minimum: 5, maximum: 255 }
+  validates :age, presence: true, numericality: { only_integer: true,greater_than_or_equal_to: 18, less_than_or_equal_to: 100 }
 
   before_create :downcase_email
 
